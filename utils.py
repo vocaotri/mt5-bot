ae5fc6355c3c40a05e58b9040c1012e7363a9ca7
BM Trisma <trisma@brickmate.vn>
2023-06-21 18:06:58 +0700

    first commit

---
+++ utils.py
@@ -0,0 +1,91 @@
+class MetaTrader5:
+    def __init__(self, mt5, mt5Id, mt5Pass, mt5Server, symbol, minLot, maxLot, slTP, orderStop):
+        self.mt5 = mt5
+        self.mt5Id = mt5Id
+        self.mt5Pass = mt5Pass
+        self.mt5Server = mt5Server
+        self.symbol = symbol
+        self.minLot = minLot
+        self.maxLot = maxLot
+        self.slTP = slTP
+        self.orderStop = orderStop
+        hasMT5 = self.mt5.initialize()
+        if hasMT5 == False:
+            print('MT5 initialize failed, install MT5 and try again')
+            quit()
+        isLogined = self.mt5.login(self.mt5Id, self.mt5Pass, self.mt5Server)
+        if isLogined == False:
+            print('Login failed')
+            quit()
+
+    def market_position(self, typeDirection='buy'):
+        action = self.mt5.TRADE_ACTION_DEAL
+        type = self.mt5.ORDER_TYPE_BUY
+        price = self.mt5.symbol_info_tick(self.symbol).ask
+        sl = self.mt5.symbol_info_tick("XAUUSD").ask - self.slTP
+        tp = self.mt5.symbol_info_tick("XAUUSD").ask + self.slTP
+        comment = 'Python script open position'
+        if typeDirection != 'buy':
+            type = self.mt5.ORDER_TYPE_SELL
+            price = self.mt5.symbol_info_tick(self.symbol).bid
+            sl = self.mt5.symbol_info_tick("XAUUSD").bid + self.slTP
+            tp = self.mt5.symbol_info_tick("XAUUSD").bid - self.slTP
+
+        request = {
+            "action": action,
+            "symbol": self.symbol,
+            "volume": self.minLot,  # FLOAT
+            "type": type,
+            "price": price,  # FLOAT
+            "sl": sl,  # FLOAT,
+            "tp": 0.0,  # FLOAT
+            "comment": comment,
+        }
+        position = self.mt5.order_send(request)
+        return position
+
+    def remove_all_position(self):
+        positions = self.mt5.positions_get()
+        for position in positions:
+            self.mt5.Close(symbol=position.symbol, ticket=position.ticket)
+
+    def order(self, typeDirection='buy'):
+        action = self.mt5.TRADE_ACTION_PENDING
+        type = self.mt5.ORDER_TYPE_BUY_LIMIT
+        price = self.mt5.symbol_info_tick(self.symbol).ask - self.orderStop
+        sl = self.mt5.symbol_info_tick("XAUUSD").ask - self.slTP
+        tp = self.mt5.symbol_info_tick("XAUUSD").ask
+        comment = 'Python script open order'
+        if typeDirection != 'buy':
+            type = self.mt5.ORDER_TYPE_SELL_LIMIT
+            price = self.mt5.symbol_info_tick(self.symbol).bid + self.orderStop
+            sl = self.mt5.symbol_info_tick("XAUUSD").bid + self.slTP
+            tp = self.mt5.symbol_info_tick("XAUUSD").bid
+
+        request = {
+            "action": action,
+            "symbol": self.symbol,
+            "volume": self.maxLot,  # FLOAT
+            "type": type,
+            "price": price,  # FLOAT
+            "sl": sl,  # FLOAT
+            "tp": tp,  # FLOAT
+            "comment": comment,
+        }
+        order = self.mt5.order_send(request)
+        return order
+
+    def cancel_all_order(self):
+        orders = self.mt5.orders_get()
+        for order in orders:
+            request = {
+                "action": self.mt5.TRADE_ACTION_REMOVE,
+                "order": order.ticket,  # select the position you want to close
+            }
+            order = self.mt5.order_send(request)
+
+    def quickTrade(self, typeDirection='buy'):
+        self.remove_all_position()
+        self.cancel_all_order()
+        self.market_position(typeDirection)
+        self.order(typeDirection)
